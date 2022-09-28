51c8de9272aec84732d9b10ead895cb9ad9102a6
Garron(嘉伦) <gejialun88@163.com>
2022-09-28 16:53:05 +0800

    ✨新增(custom): code

--- packages/vchart/docs/.vitepress/theme/playground/template.jsx
+++
@@ -1,8 +0,0 @@
-import { Button } from 'promiseui-vue';
-import { defineComponent, ref } from 'vue';
-export default defineComponent({
-  setup() {
-    const msg = ref('hello jsx!');
-    return () => <Button>{msg.value}</Button>;
-  }
-});
--- packages/vchart/docs/plots/area/demo1/demo1.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <button>按钮一</button>
-    <button>按钮二</button>111
-  </div>
-</template>
